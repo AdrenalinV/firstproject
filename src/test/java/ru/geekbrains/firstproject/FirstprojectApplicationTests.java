edd74f36a38c7cf6edda42b59760dafd49b29287
VadimHome <vadimtsh@gmail.com>
2021-07-08 22:45:22 +0300

    Initial commit

---
+++ src/test/java/ru/geekbrains/firstproject/FirstprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.firstproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
